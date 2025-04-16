4f627205116ab1747df779e14cb57a8f90d089f8
KareemHassan <kareem99710@gmail.com>
2025-04-16 17:09:35 +0200

    Add basic struct for player

--- Game.cpp
+++ Game.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
-int main(){
- cout << "Hello oumda" << endl;
-  
+
+struct player {
+	int Row, Col; //Position
+	int Health;
+	int coins; //Either coins or Level , until decided i will leave it as coins
+	char currentWeapon[]; //Either gun (bullets) or Lazerbeam (lazer)
+	int ammo , maxAmmo; // (ammo/maxAmmo) , max ammo could be 24 for gun , (maxammo == 1) for lazerbeam
+	int Reload[2] = {1 , 3}; //coolDown[0] for gun , coolDown[1] for lazerbeam
+};
+
+int main() {
+	int width = 80, height = 24;
 }
