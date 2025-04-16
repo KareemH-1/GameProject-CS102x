f01419a67aac30e708339348f566546d5b54691e
KareemHassan <kareem99710@gmail.com>
2025-04-16 17:12:06 +0200

    Refactor player struct: replace currentWeapon with gun and update comments for clarity

--- Game.cpp
+++ Game.cpp
@@ -5,11 +5,12 @@ struct player {
 	int Row, Col; //Position
 	int Health;
 	int coins; //Either coins or Level , until decided i will leave it as coins
-	char currentWeapon[]; //Either gun (bullets) or Lazerbeam (lazer)
+	int gun; //Either 1 for gun (bullets) or 2 for Lazerbeam (lazer)
 	int ammo , maxAmmo; // (ammo/maxAmmo) , max ammo could be 24 for gun , (maxammo == 1) for lazerbeam
 	int Reload[2] = {1 , 3}; //coolDown[0] for gun , coolDown[1] for lazerbeam
 };
 
 int main() {
-	int width = 80, height = 24;
+	int width = 80, height = 24; // Lets keep the width and height stored as variables incase we want to change them later
+    char board[height][width]; // Border for the game frame
 }
