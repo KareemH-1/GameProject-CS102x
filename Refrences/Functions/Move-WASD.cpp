fbc3052e8de4e917f0350de9d8e01c64c6ef840f
MazenMDev <mazen.m.mahmoud0@gmail.com>
2025-04-16 23:11:50 +0200

    feat: implemented basic character movement with WASD keys

--- Refrences/Functions/Move-WASD.cpp
+++ Refrences/Functions/Move-WASD.cpp
@@ -1,7 +1,50 @@
 #include <iostream>
+#include <conio.h>
+#include <windows.h>
 using namespace std;
-int main(){
+
+void moveplayer(char move_dir, int& player_y, int& player_x) {
+    if (move_dir == 'w')    player_y--;
+    else if (move_dir == 's')    player_y++;
+    else if (move_dir == 'a')    player_x--;
+    else if (move_dir == 'd')    player_x++;
+}
+
+void draw_player(int player_y, int player_x) {
+    system("cls");
+    for (int r = 0; r < 24; r++) {
+        for (int c = 0; c < 80; c++) {
+            if (c == player_x && r == player_y)
+            {
+                cout << (char)233;
+            }
+            else
+            {
+                cout << ' ';
+            }
+        }
+        cout << endl;
+    }
+}
 
 
-    
+int main() {
+    int player_y = 10;
+    int player_x = 10;
+    char key;
+
+    draw_player(player_y,player_x);
+    while (true)
+    {
+        if (_kbhit())
+        {
+            key = _getch();
+            if (key == 'w' || key == 's' || key == 'a' || key == 'd')
+            {
+                moveplayer(key, player_y, player_x);
+                draw_player(player_y, player_x);
+            }
+        }
+        Sleep(50);
+    }
 }
