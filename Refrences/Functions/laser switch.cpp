4f18f0524db802eb02b366cbc631b0ac4079f711
MazenMDev <mazen.m.mahmoud0@gmail.com>
2025-04-22 05:29:55 +0200

    - Add Enemy struct with health and behavior - Implement basic menu for controls, mode selection, and ammo/health display

--- Refrences/Functions/laser switch.cpp
+++ Refrences/Functions/laser switch.cpp
@@ -3,72 +3,102 @@
 #include <windows.h>  // For Sleep() function to pause the game
 using namespace std;
 
-
 char board[24][80];
 
+struct Enemy {
+    int health = 100;
+    int row = 15;
+    int col = 50;
 
-
-struct Laser {
-    void shootlaser() {
-        int posr, posc;
-        for (int r = 0; r < 24; r++) {
-            for (int c = 0; c < 80; c++) {
-                if (board[r][c] == (char)254)
-                {
-                    posr = r;
-                    posc = c;
-                }
-
-            }
-        }
-        int h = 1;
-        while (board[posr][posc + h] == ' ' && h < 20)
-        {
-            board[posr][posc + h] = '=';
-            h++;
-
-            system("cls");
-            for (int r = 0; r < 24; r++) {
-                for (int c = 0; c < 80; c++) {
-
-                    cout << board[r][c];
-
-                }
-                cout << endl;
-            }
-        }
-        int k = h;
-        h = 1;
-        while (true) {
-
-            if (board[posr][posc + h] == '=')
-            {
-                board[posr][posc + h] = ' ';
-            }
-            system("cls");
-            for (int r = 0; r < 24; r++) {
-                for (int c = 0; c < 80; c++) {
-
-                    cout << board[r][c];
-
-                }
-                cout << endl;
-            }
-            h++;
-            if (h == k)
-            {
-                break;
-            }
-        }
-
-
-
+    void createEnemy(int row, int col) {
+        this->row = row;
+        this->col = col;
+        board[row - 3][col + 10] = '_';
+        board[row - 3][col + 11] = '_';
+        board[row - 2][col + 7] = '<';
+        board[row - 2][col + 8] = '(';
+        board[row - 2][col + 9] = 'o';
+        board[row - 2][col + 11] = ')';
+        board[row - 2][col + 12] = '_';
+        board[row - 2][col + 13] = '_';
+        board[row - 2][col + 14] = '_';
+        board[row - 1][col + 8] = '(';
+        board[row - 1][col + 13] = '.';
+        board[row - 1][col + 14] = '_';
+        board[row - 1][col + 15] = '>';
+        board[row - 0][col + 9] = '`';
+        board[row - 0][col + 10] = '-';
+        board[row - 0][col + 11] = '-';
+        board[row - 0][col + 12] = '-';
+        board[row - 0][col + 13] = '-';
+        board[row - 0][col + 14] = '\'';
     }
 };
 
-struct Ammo
-{
+struct Laser {
+    void shootLaser(Enemy& enemy, int& killed) {
+        int posr = -1, posc = -1;
+        for (int r = 0; r < 24; r++) {
+            for (int c = 0; c < 80; c++) {
+                if (board[r][c] == (char)254) {
+                    posr = r;
+                    posc = c;
+                }
+            }
+        }
+
+        if (posr != -1 && posc != -1) {
+            int h = 1;
+            while (board[posr][posc + h] == ' ' && h < 20) {
+                board[posr][posc + h] = '=';
+
+                // Check for collision with the enemy
+                if (posr == enemy.row - 1 && posc + h >= enemy.col && posc + h <= enemy.col + 14) {
+                    enemy.health -= 10;  // Laser deals 10 damage
+                    cout << "Enemy hit! Health: " << enemy.health << endl;
+                    if (enemy.health == 0)
+                    {
+                        cout << "you killed the duck :(" << endl;
+                        killed = 1;
+                    }
+                }
+
+                h++;
+                system("cls");
+                for (int r = 0; r < 24; r++) {
+                    for (int c = 0; c < 80; c++) {
+                        cout << board[r][c];
+                    }
+                    cout << endl;
+                }
+            }
+
+            // Reset laser position
+            int k = h;
+            h = 1;
+            while (true) {
+                if (board[posr][posc + h] == '=') {
+                    board[posr][posc + h] = ' ';
+                }
+                system("cls");
+                for (int r = 0; r < 24; r++) {
+                    for (int c = 0; c < 80; c++) {
+                        cout << board[r][c];
+                    }
+                    cout << endl;
+                }
+                h++;
+                if (h == k) {
+                    break;
+                }
+            }
+        }
+    }
+};
+
+struct Ammo {
     int count = 10;
+
     void use() {
         if (count > 0) {
             count--;
@@ -78,6 +108,7 @@ struct Ammo
             cout << "Ammo is out!" << endl;
         }
     }
+
     void reload(int amount) {
         count += amount;
         cout << "Ammo reloaded! Current ammo: " << count << endl;
@@ -85,68 +116,71 @@ struct Ammo
 };
 
 struct Gun {
-    void shootGun() {
-        int posr;
-        int posc;
+    void shootGun(Enemy& enemy, int &killed) {
+        int posr = -1, posc = -1;
         for (int r = 0; r < 24; r++) {
             for (int c = 0; c < 80; c++) {
-                if (board[r][c] == (char)254)
-                {
+                if (board[r][c] == (char)254) {
                     posr = r;
                     posc = c;
                 }
             }
         }
 
-
-        int r = posr + 1;
-        int ct = 0;
-        int h = 1;
-        for (int c = posc + 1; c < 80; c++) {
-            if (board[r][c] == ' ')
-            {
-                board[r][c] = 'o';
-            }
-            else
-            {
-                h = -1;
-            }
-
-            system("cls");
-            for (int i = 0; i < 24; i++) {
-                for (int j = 0; j < 80; j++) {
-                    cout << board[i][j];
+        if (posr != -1 && posc != -1) {
+            int r = posr + 1;
+            int ct = 0;
+            int h = 1;
+            for (int c = posc + 1; c < 80; c++) {
+                if (board[r][c] == ' ') {
+                    board[r][c] = 'o';
                 }
-                cout << endl;
-            }
+                else {
+                    h = -1;
+                }
 
-            Sleep(50);
-            if (board[r][c] == 'o')
-            {
-                board[r][c] = ' ';
-            }
-            if (h == 1)
-                r++;
-            else r--;
+                // Check for collision with the enemy
+                if (r == enemy.row && c >= enemy.col && c <= enemy.col + 14) {
+                    enemy.health -= 10;  // Gun deals 10 damage
+                    cout << "Enemy hit! Health: " << enemy.health << endl;
+                    if (enemy.health == 0)
+                    {
+                        cout << "you killed the duck :(" << endl;
+                        killed = 1;
+                    }
+                }
 
-            ct++;
-            if (ct == 11)
-                break;
+                system("cls");
+                for (int i = 0; i < 24; i++) {
+                    for (int j = 0; j < 80; j++) {
+                        cout << board[i][j];
+                    }
+                    cout << endl;
+                }
+
+                Sleep(50);
+                if (board[r][c] == 'o') {
+                    board[r][c] = ' ';
+                }
+                if (h == 1) r++;
+                else r--;
+
+                ct++;
+                if (ct == 11)
+                    break;
+            }
         }
     }
-
-
 };
 
-struct Player
-{
+struct Player {
     int health = 100;
     Ammo ammo;
     Gun gun;
     Laser laser;
 };
 
-//The main function
+// The main function
 int main() {
     for (int i = 0; i < 24; i++) {
         for (int j = 0; j < 80; j++) {
@@ -169,6 +203,8 @@ int main() {
     board[23][79] = '#';
     board[23][0] = '#';
 
+    Enemy enemy;
+    enemy.createEnemy(16, 50);
 
     board[15][40] = (char)254; // the laser
     for (int r = 0; r < 24; r++) {
@@ -178,58 +214,72 @@ int main() {
         cout << endl;
     }
 
+
+
     Player player;
 
     char hit;
     int ct = 0;
+    int killed = 0;
     for (;;) {
-        hit = _getch();  // Get the pressed 
-        if (_kbhit)
-        {
-            if (hit == 't')
-            {
-                ct++;
+        system("cls");
+        // Redraw the board
+        for (int r = 0; r < 24; r++) {
+            for (int c = 0; c < 80; c++) {
+                cout << board[r][c];
             }
-            if (ct % 2 == 0)
-            {
-                cout << "Laser mode" << endl;
-                if (hit == 'f')
-                {
-                    if (player.ammo.count > 0) {
-                        player.laser.shootlaser();
-                        player.ammo.use();
-                    }
-                    else {
-                        cout << "Out of ammo!" << endl;
-                    }
-                }
-            }
+            cout << endl;
+        }
 
-            if (ct % 2 != 0)
-            {
-                cout << "Gun mode" << endl;
-                if (hit == 'f')
-                {
-                    if (player.ammo.count > 0) {
-                        player.gun.shootGun();
-                        player.ammo.use();
-                    }
-                    else {
-                        cout << "Out of ammo!" << endl;
-                    }
+        cout << "Enemy Health: " << enemy.health << endl;
+        cout << "Ammo: " << player.ammo.count << endl;
+        if (ct % 2 == 0)
+            cout << "laser mode" << endl;
+        else
+            cout << "Gun mode" << endl;
+        cout << "Controls: F (fire), T (toggle weapon), R (reload), Q (quit)" << endl;
+
+        hit = _getch();  // Get the pressed key
+        if (hit == 't') {
+            ct++;
+        }
+
+        if (ct % 2 == 0) {
+            cout << "Laser mode" << endl;
+            if (hit == 'f') {
+                if (player.ammo.count > 0) {
+                    player.laser.shootLaser(enemy, killed);
+                    player.ammo.use();
+
                 }
-            }
-            if (hit == 'r') {  
-                player.ammo.reload(5);
-            }
-            if (hit == 'q') {
-                cout << "\nExiting game... Goodbye!" << endl;
-                break;
+                else {
+                    cout << "Out of ammo!" << endl;
+                }
             }
         }
 
+        if (ct % 2 != 0) {
+            cout << "Gun mode" << endl;
+            if (hit == 'f') {
+                if (player.ammo.count > 0) {
+                    player.gun.shootGun(enemy, killed);
+                    player.ammo.use();
+                }
+                else {
+                    cout << "Out of ammo!" << endl;
+                }
+            }
+        }
+
+        if (hit == 'r') {
+            player.ammo.reload(5);
+        }
+
+        if (hit == 'q' || killed == 1) {
+            cout << "\nExiting game... Goodbye!" << endl;
+            break;
+        }
     }
 
+    return 0;
 }
-
-
