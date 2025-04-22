53d6b6e961dee416015c4e187accaf59e37c2d45
AhmedEmad375 <medo007bond@gmail.com>
2025-04-22 02:06:34 +0200

    ladder

--- Game Functions/ladder.cpp
+++ Game Functions/ladder.cpp
@@ -2,31 +2,90 @@
 #include<conio.h>
 using namespace std;
 void drawladder(char board[24][80], int row, int col) {
-    //retard ladder under construction still
-    board[row - 1][col + 4] = '|';
-    board[row - 2][col + 4] = '|';
-    board[row - 3][col + 4] = '|';
-    board[row - 4][col + 4] = '|';
-    board[row - 5][col + 4] = '|';
-    board[row - 6][col + 4] = '|';
-    board[row - 7][col + 4] = '|';
-    board[row - 8][col + 4] = '|';
-    board[row - 9][col + 4] = '|';
-    board[row - 9][col + 3] = '_';
-    board[row - 9][col + 2] = '_';
-    board[row - 6][col + 3] = '_';
-    board[row - 6][col + 2] = '_';
-    board[row - 3][col + 3] = '_';
-    board[row - 6][col + 2] = '_';
-    board[row - 1][col + 1] = '|';
-    board[row - 2][col + 1] = '|';
-    board[row - 3][col + 1] = '|';
-    board[row - 4][col + 1] = '|';
-    board[row - 5][col + 1] = '|';
-    board[row - 6][col + 1] = '|';
-    board[row - 7][col + 1] = '|';
-    board[row - 8][col + 1] = '|';
-    board[row - 9][col + 1] = '|';
+    void drawladder(char board[24][80], int row, int col) {
+        //retard ladder under construction still
+    
+        board[row - 1][col] = '|';
+        board[row - 2][col] = '|';
+        board[row - 3][col] = '|';
+        board[row - 4][col] = '|';
+        board[row - 5][col] = '|';
+        board[row - 6][col] = '|';
+        board[row - 7][col] = '|';
+        board[row - 8][col] = '|';
+        board[row - 9][col] = '|';
+        
+        board[row - 1][col + 1] = '|';
+        board[row - 2][col + 1] = '|';
+        board[row - 3][col + 1] = '|';
+        board[row - 4][col + 1] = '|';
+        board[row - 5][col + 1] = '|';
+        board[row - 6][col + 1] = '|';
+        board[row - 7][col + 1] = '|';
+        board[row - 8][col + 1] = '|';
+        board[row - 9][col + 1] = '|';
+    
+        board[row - 1][col + 2] = '-';
+        board[row - 1][col + 3] = '-';
+        board[row - 1][col + 4] = '-';
+    
+        board[row - 2][col + 2] = '-';
+        board[row - 2][col + 3] = '-';
+        board[row - 2][col + 4] = '-';
+    
+    
+        board[row - 3][col + 2] = '-';
+        board[row - 3][col + 3] = '-';
+        board[row - 3][col + 4] = '-';
+    
+        board[row - 4][col + 2] = '-';
+        board[row - 4][col + 3] = '-';
+        board[row - 4][col + 4] = '-';
+    
+        board[row - 5][col + 2] = '-';
+        board[row - 5][col + 3] = '-';
+        board[row - 5][col + 4] = '-';
+        
+    
+        board[row - 6][col + 2] = '-';
+        board[row - 6][col + 3] = '-';
+        board[row - 6][col + 4] = '-';
+    
+        board[row - 7][col + 2] = '-';
+        board[row - 7][col + 3] = '-';
+        board[row - 7][col + 4] = '-';
+    
+    
+        board[row - 8][col + 2] = '-';
+        board[row - 8][col + 3] = '-';
+        board[row - 8][col + 4] = '-';
+    
+        board[row - 9][col + 2] = '-';
+        board[row - 9][col + 3] = '-';
+        board[row - 9][col + 4] = '-';
+    
+    
+        board[row - 1][col + 5] = '|';
+        board[row - 2][col + 5] = '|';
+        board[row - 3][col + 5] = '|';
+        board[row - 4][col + 5] = '|';
+        board[row - 5][col + 5] = '|';
+        board[row - 6][col + 5] = '|';
+        board[row - 7][col + 5] = '|';
+        board[row - 8][col + 5] = '|';
+        board[row - 9][col + 5] = '|';
+    
+        board[row - 1][col + 6] = '|';
+        board[row - 2][col + 6] = '|';
+        board[row - 3][col + 6] = '|';
+        board[row - 4][col + 6] = '|';
+        board[row - 5][col + 6] = '|';
+        board[row - 6][col + 6] = '|';
+        board[row - 7][col + 6] = '|';
+        board[row - 8][col + 6] = '|';
+        board[row - 9][col+6]='|';
+    
+    }
 }
 
 
