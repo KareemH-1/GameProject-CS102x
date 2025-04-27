b6bd5ebc8dff192c93bf13ca9312859e985a2d42
KareemHassan <kareem99710@gmail.com>
2025-04-27 18:15:39 +0300

    Add dynamic length for ladder

--- Game Functions/ladder.cpp
+++ Game Functions/ladder.cpp
@@ -1,7 +1,32 @@
 #include<iostream>
 #include<conio.h>
 using namespace std;
-    void drawladder(char board[24][80], int row, int col) {
+
+
+void drawLadderDependingOnLength(char board[24][80], int row, int col , int length) {
+
+    for (int i = 0; i < length; i++) {
+        board[row - i][col] = '|';
+        board[row - i][col + 1] = '|';
+
+        board[row - i][col + 2] = '-';
+        board[row - i][col + 3] = '-';
+        board[row - i][col + 4] = '-';
+        board[row - i][col + 5] = '-';
+        board[row - i][col + 6] = '-';
+        board[row - i][col + 7] = '-';
+        board[row - i][col + 8] = '-';
+        board[row - i][col + 9] = '-';
+        board[row - i][col + 10] = '-';
+        board[row - i][col + 11] = '-';
+
+        board[row - i][col + 12] = '|';
+        board[row - i][col + 13] = '|';
+    }
+
+}   
+
+void drawladder(char board[24][80], int row, int col) {
         //retard ladder under construction still
 
         board[row - 1][col] = '|';
@@ -83,49 +108,37 @@ using namespace std;
         board[row - 7][col + 6] = '|';
         board[row - 8][col + 6] = '|';
         board[row - 9][col + 6] = '|';
+}
 
-        void climp(char board[24][80], int& posJHero, int& posIHero, int widthHero, int heightHero, int col,int row) {
+            
+void climp(char board[24][80], int& posJHero, int& posIHero, int widthHero, int heightHero, int col,int row) {
             int check = 1;
             // Check every cell in the column to the right of the player
             for (int i = posIHero; i >= posIHero - heightHero + 1; i--) {
                 if (posJHero <=col + 4 && posJHero >= col) {
                     if(getch()=='w'){
-                        if (posiHero + 1 <=row-1) {
+                        if (posIHero + 1 <=row-1) {
                             
-                            posihero--;
+                            posIHero--;
                         }
                     }
                     
                 }
             }
-        }
-        void climp(char board[24][80], int& posJHero, int& posIHero, int widthHero, int heightHero, int col,int row) {
-            int check = 1;
-            // Check every cell in the column to the right of the player
-            for (int i = posIHero; i >= posIHero - heightHero + 1; i--) {
-                if (posJHero <=col + 4 && posJHero >= col) {
-                    if(getch()=='w'){
-                        if (posiHero + 1 <=row-1) {
-                            
-                            posihero--;
-                        }
-                    }
-                    
-                }
-            }
-        }   
+           
         char board[24][80] = { ' ' };
         int row = 10, col = 10;
         drawladder(board, row, col);
+      
         for (int i = 0; i < 24; i++) {
             for (int j = 0; j < 80; j++) {
                 cout << board[i][j];
             }
             cout << endl;
         }
+
+}
     
-    }
-   
 
     
     
