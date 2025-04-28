e41b602dcac44d279e5cbee4ac3792665616f8a5
KareemHassan <kareem99710@gmail.com>
2025-04-28 10:11:02 +0300

    helo

--- Game Functions/Elevator.cpp
+++ Game Functions/Elevator.cpp
@@ -1,32 +1,40 @@
-#include <iostream>
-#include <conio.h>
 
-using namespace std;
+void moveElevatorVertically(int& row, int startRow, int endRow, int& direction) {
+	if (row == endRow) direction = 2;
+	else if (row == startRow) direction = 1;
 
-void moveElevatorVertically (int& row , int startRow , int endRow  , int &direction){
-	if(row == endRow) direction = 2;
-	else if(row == startRow) direction =1;
-	
 	if (direction == 1) row--;
 	else row++;
 }
 
-void moveElevatorHorizontally(int & col , int startCol , int endCol , int & direction){
-	if(col == endCol) direction = 2;
-	else if(col == startCol) direction =1;
-	
+void moveElevatorHorizontally(int& col, int startCol, int endCol, int& direction) {
+	if (col == endCol) direction = 2;
+	else if (col == startCol) direction = 1;
+
 	if (direction == 1) col++;
 	else col--;
 }
-void drawElevator(char board[100][1000] , int row , int col){
-	board[row][col] = '_' ;
-	board[row][col+1] = '_' ;
-	board[row][col+2] = '_' ;
-	board[row][col+4] = '_' ;
-	board[row][col+5] = '_' ;
-	board[row][col+6] = '_' ;
-	board[row][col+7] = '_' ;
-	board[row][col+8] = '_' ;
-	board[row][col+9] = '_' ;
-	board[row][col+10] = '_' ;
+void drawElevator(char board[100][1000], int row, int col) {
+	board[row][col] = '_';
+	board[row][col + 1] = '_';
+	board[row][col + 2] = '_';
+	board[row][col + 4] = '_';
+	board[row][col + 5] = '_';
+	board[row][col + 6] = '_';
+	board[row][col + 7] = '_';
+	board[row][col + 8] = '_';
+	board[row][col + 9] = '_';
+	board[row][col + 10] = '_';
+}
+
+void drawAndMoveElevatorV(char board[100][1000], int& row , int col, int startRow, int endRow , int& direction) {
+	
+	drawElevator(board, row, col);
+	moveElevatorVertically(row, startRow, endRow, direction);
+}
+
+void drawAndMoveElevatorH(char board[100][1000], int row, int &col, int startCol, int endCol, int& direction) {
+
+	drawElevator(board, row, col);
+	moveElevatorVertically(row, startCol, endCol, direction);
 }
