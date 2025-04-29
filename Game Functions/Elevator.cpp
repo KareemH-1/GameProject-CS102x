394bbf6080b195ea85f555722f6b5099e56c5a3c
AhmedEmad375 <medo007bond@gmail.com>
2025-04-29 08:23:19 +0300

    elevate

--- Game Functions/Elevator.cpp
+++ Game Functions/Elevator.cpp
@@ -38,3 +38,24 @@ void drawAndMoveElevatorH(char board[100][1000], int row, int &col, int startCol
 	drawElevator(board, row, col);
 	moveElevatorVertically(row, startCol, endCol, direction);
 }
+
+void moveplayervertically(int& row, int startRow, int endRow, int& direction) {
+	if (row == endRow) direction = 2;
+	else if (row == startRow+1) direction = 1;
+
+	if (direction == 1) row--;
+	else row++;
+}
+
+
+void moveplayerhorizontally(int& col, int startCol, int endCol, int& direction) {
+	if (col == endCol) direction = 2;
+	else if (col == startCol) direction = 1;
+
+	if (direction == 1) col++;
+	else col--;
+}
+
+void playerElevate (char board[100][1000], &pX, &pY,int row, int &col,int& direction){
+
+}
