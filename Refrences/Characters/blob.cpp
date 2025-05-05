673edc04ed7e204ba631cc9b9cd5dc73e428da3b
KareemHassan <kareem99710@gmail.com>
2025-05-06 00:00:48 +0300

    Add blob drawing function

--- Refrences/Characters/blob.cpp
+++ Refrences/Characters/blob.cpp
@@ -29,3 +29,147 @@ void moveBlob( int& row , int& col , int startCol , int endCol , int &direction 
         
     }
 }
+
+void drawBlob(char board[100][1000] , int row , int col , int direction){
+    
+    
+    if (direction == 1) {
+        board[row][col+3] = '|';
+        board[row][col+4] = '_';
+        board[row][col+5] = '|';
+
+        board[row][col+8] = '|';
+        board[row][col+9] = '_';
+        board[row][col+10] = '|';
+
+        board[row-1][col+2] = '\\';
+        board[row-1][col+3] = '_';
+        board[row-1][col+4] = '_';
+        board[row-1][col+5] = '_';
+        board[row-1][col+6] = '_';
+        board[row-1][col+7] = '_';
+        board[row-1][col+8] = '_';
+        board[row-1][col+9] = '_';
+        board[row-1][col+10] = '_';
+        board[row-1][col+11] = '/';
+
+        board[row-1][col+13] = '`';
+    
+
+        board[row-2][col+2] = '|';
+        board[row-2][col+3] = ' ';
+        board[row-2][col+4] = '_';
+        board[row-2][col+5] = '_';
+        board[row-2][col+6] = '_';
+        board[row-2][col+7] = ' ';
+        board[row-2][col+8] = ' ';
+        board[row-2][col+9] = ' ';
+        board[row-2][col+10] = ' ';
+        board[row-2][col+11] = '|';
+        board[row-2][col+12] = '\\';
+
+        
+        board[row-3][col+1] = '\\';
+        board[row-3][col+2] = '|';
+        board[row-3][col+3] = ' ';
+        board[row-3][col+4] = 'O';
+        board[row-3][col+5] = ' ';
+        board[row-3][col+6] = 'O';
+        board[row-3][col+7] = ' ';
+        board[row-3][col+8] = ' ';
+        board[row-3][col+9] = ' ';
+        board[row-3][col+10] = ' ';
+        board[row-3][col+11] = '|';
+
+        board[row-4][col] = '|';
+        board[row-4][col+3] = '/';
+        board[row-4][col+4] = ' ';
+        board[row-4][col+5] = ' ';
+        board[row-4][col+6] = ' ';
+        board[row-4][col+7] = ' ';
+        board[row-4][col+8] = ' ';
+        board[row-4][col+9] = ' ';
+        board[row-4][col+10] = '\\';
+        
+        board[row-5][col+4] = '_';
+        board[row-5][col+5] = '_';
+        board[row-5][col+6] = '_';
+        board[row-5][col+7] = '_';
+        board[row-5][col+8] = '_';
+        board[row-5][col+9] = '_';
+    
+    
+    }
+    
+
+
+    
+    else if (direction == 2) {
+    
+        
+        board[row][col+3] = '|';
+        board[row][col+4] = '_';
+        board[row][col+5] = '|';
+
+        board[row][col+8] = '|';
+        board[row][col+9] = '_';
+        board[row][col+10] = '|';
+
+        board[row-1][col+2] = '/';
+        board[row-1][col+3] = '_';
+        board[row-1][col+4] = '_';
+        board[row-1][col+5] = '_';
+        board[row-1][col+6] = '_';
+        board[row-1][col+7] = '_';
+        board[row-1][col+8] = '_';
+        board[row-1][col+9] = '_';
+        board[row-1][col+10] = '_';
+        board[row-1][col+11] = '\\';
+
+        board[row-1][col+13] = '\'';
+
+        board[row-2][col+2] = '/';
+        board[row-2][col+3] = '|';
+        board[row-2][col+4] = ' ';
+        board[row-2][col+5] = ' ';
+        board[row-2][col+6] = ' ';
+        board[row-2][col+7] = '_';
+        board[row-2][col+8] = '_';
+        board[row-2][col+9] = '_';
+        board[row-2][col+10] = ' ';
+        board[row-2][col+11] = ' ';
+        board[row-2][col+12] = '|';
+
+        board[row-3][col+1] = '|';
+        board[row-3][col+2] = ' ';
+        board[row-3][col+3] = ' ';
+        board[row-3][col+4] = ' ';
+        board[row-3][col+5] = ' ';
+        board[row-3][col+6] = 'O';
+        board[row-3][col+7] = ' ';
+        board[row-3][col+8] = 'O';
+        board[row-3][col+9] = ' ';
+        board[row-3][col+10] = '|';
+        board[row-3][col+11] = '/';
+
+        board[row-4][col] = '\\';
+        board[row-4][col+1] = ' ';
+        board[row-4][col+2] = ' ';
+        board[row-4][col+3] = ' ';
+        board[row-4][col+4] = ' ';
+        board[row-4][col+5] = ' ';
+        board[row-4][col+6] = ' ';
+        board[row-4][col+7] = '\\';
+        board[row-4][col+10] = '|';
+
+        board[row-5][col+4] = '_';
+        board[row-5][col+5] = '_';
+        board[row-5][col+6] = '_';
+        board[row-5][col+7] = '_';
+        board[row-5][col+8] = '_';
+        board[row-5][col+9] = '_';
+
+
+    }
+
+}
